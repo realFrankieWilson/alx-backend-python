f404b212779e1a565edb0fa7192a64417f07a44c
realFrankieWilson <frankuwill101@gmail.com>
2024-06-14 18:35:59 +0100

    Added task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Module `2-floor`
+This module provides a type-annotated function that returns
+the string representation of a float
+
+Function:
+  to_str(n: float) -> str:
+      Converts a float to a string and return the result.
+
+      Arg:
+          n (float): an argument of type float.
+
+      Returns: The string representation of the float `n`.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Converts a float to a string and returns a string value.
+
+    Args:
+        n (int): the float value.
+
+    returns:
+        A string value.
+    """
+    return str(n)
